b8b480f986406559002875b530669a1073856fc8
theodorbrown <theodor.brown01@gmail.com>
2021-09-13 15:19:54 +0200

    maj

--- index.js
+++ index.js
@@ -5,3 +5,10 @@ require('dotenv').config();
 
 //port d'écoute
 const port = process.env.PORT || 3000;
+
+const express = require('express');
+const app = express();
+
+app.listen(port, () => {
+  console.log(`Server is running on ${port} port.`);
+})
